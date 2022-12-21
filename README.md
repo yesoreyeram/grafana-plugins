9987a144bae4b0e55adf6732297f19c65518bf32
Sriramajeyam Sugumaran <yesoreyeram@gmail.com>
2022-12-21 11:39:04 +0000

    update outdated actions

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # Grafana Plugins
 
-My experiments with Grafana plugins. This is a mono-repo of multiple grafana related plugins and packages.
+My experiments with [Grafana](https://grafana.com) [plugins](https://grafana.com/plugins) eco-system. This is a mono-repo of following plugins and grafana plugins related packages.
 
 ## Included plugins
 
+- [yesoreyeram-blank-datasource](./plugins/yesoreyeram-blank-datasource/)
 - [yesoreyeram-hello-datasource](./plugins/yesoreyeram-hello-datasource/)
-- [yesoreyeram-zero-datasource](./plugins/yesoreyeram-zero-datasource/)
 
 ## Included go packages
 
--- cypress/e2e/flows/addDatasource.ts
+++ cypress/e2e/flows/addDatasource.ts
@@ -1,5 +1,24 @@
 import { get, contains, visit, log, wait } from "../utils/cy";
 
+const selector = {
+  DSList: {
+    Url: "/datasources",
+    DSTitle: "h2 a",
+  },
+  DSEdit: {
+    TestButton: "button:last-child",
+    ValidationMessage: `[aria-label="Data source settings page Alert"]`,
+  },
+  DSNew: {
+    Url: "/datasources/new",
+    PluginTile: (pluginName: string) =>
+      `[aria-label$="data source ${pluginName}"]`,
+    NameInput: `[aria-label="Data source settings page name input field"]`,
+    SaveAndTestButton: `[aria-label="Data source settings page Save and Test button"]`,
+    ValidationMessage: `[aria-label="Data source settings page Alert"]`,
+  },
+};
+
 export const addDataSource = (
   pluginName: string = "TestData DB",
   name = new Date().getTime().toLocaleString(),
@@ -7,19 +26,17 @@ export const addDataSource = (
   timeout = 30 * 1000
 ) => {
   log(`Adding datasource ${pluginName} - ${name}`);
-  visit("/datasources/new");
+  visit(selector.DSNew.Url);
   contains("Add data source");
-  get(`[aria-label$="data source ${pluginName}"]`)
+  get(selector.DSNew.PluginTile(pluginName))
     .contains(pluginName)
     .scrollIntoView()
     .should("be.visible")
     .click();
   contains(`Type: ${pluginName}`);
-  get(`[aria-label="Data source settings page name input field"]`)
-    .clear()
-    .type(`${pluginName}-${name}`);
-  get(`[aria-label="Data source settings page Save and Test button"]`).click();
-  get(`[aria-label="Data source settings page Alert"]`)
+  get(selector.DSNew.NameInput).clear().type(`${pluginName}-${name}`);
+  get(selector.DSNew.SaveAndTestButton).click();
+  get(selector.DSNew.ValidationMessage)
     .should("exist")
     .contains(expectedMessage, { timeout });
 };
@@ -31,20 +48,20 @@ export const validateProvisionedDatasource = (
   timeout = 30 * 1000
 ) => {
   log(`Validating provisioned datasource - ${name}`);
-  visit("/datasources");
-  get("h2 a").each(($v) => {
+  visit(selector.DSList.Url);
+  get(selector.DSList.DSTitle).each(($v) => {
     if ($v.text() === name) {
       cy.wrap($v).contains(name).scrollIntoView().should("be.visible").click();
     }
   });
   wait(2 * 1000);
   (checks || []).forEach((c) => contains(c));
-  get("button:last-child")
+  get(selector.DSEdit.TestButton)
     .contains("Test")
     .scrollIntoView()
     .should("be.visible")
     .click();
-  get(`[aria-label="Data source settings page Alert"]`)
+  get(selector.DSEdit.ValidationMessage)
     .should("exist")
     .contains(expectedMessage, { timeout });
 };
